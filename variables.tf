e127c5fb60a2ff73b18abd5b79239af487b4f0ff
Rahul Nagpure <rahulnagpure@gmail.com>
2026-04-22 22:31:32 +1000

    tesitng

--- variables.tf
+++ variables.tf
@@ -1,2 +1,10 @@
-variable "environment" { type = string }
-variable "name_suffix" { type = string }
+variable "name_suffix" {
+  description = "Suffix for unique bucket naming per environment or PR"
+  type        = string
+  default     = ""
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
