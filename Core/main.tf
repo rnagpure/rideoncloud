646003bb88dcdee6d63458d2cbbc5d3b6a19e2a9
Rahul Nagpure <rahulnagpure@gmail.com>
2026-05-06 21:26:32 +1000

    tesitng

--- Core/main.tf
+++ Core/main.tf
@@ -1,7 +1,7 @@
 module "s3_core_app" {
   source = "../modules/s3"
 
-  bucket_name        = "coredata1"
+  bucket_name        = "coredata2"
   environment        = var.environment
   name_suffix        = var.name_suffix
   versioning_enabled = true
